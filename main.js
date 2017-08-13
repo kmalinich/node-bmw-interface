0ededc6adcd4b3532488a3a143ad85274215df9a
kdm <kennygprs@gmail.com>
2017-08-13 15:16:22 -0400

    Update

--- main.js
+++ main.js
@@ -32,13 +32,13 @@ function term_config(pass) {
 	process.on('SIGTERM', () => {
 		console.log('');
 		log.msg({ msg : 'Caught SIGTERM' });
-		term();
+		process.nextTick(term);
 	});
 
 	process.on('SIGINT', () => {
 		console.log('');
 		log.msg({ msg : 'Caught SIGINT' });
-		term();
+		process.nextTick(term);
 	});
 
 	process.on('exit', () => {
@@ -50,6 +50,9 @@ function term_config(pass) {
 
 // Render serialport options object
 function serial_options(parity, collision_detection) {
+	// DBUS+IBUS+KBUS :  9600 8e1
+	// USB serial LCD : 57600 8n1
+
 	// Trick Daddy - I'm a Thug
 	let baud_rate;
 	switch (parity) {
@@ -86,20 +89,10 @@ function serial_options(parity, collision_detection) {
 // Function to load modules that require data from config object,
 // AFTER the config object is loaded
 function load_modules(pass) {
-	// Vehicle data bus protocol config
-	protocol = {
-		config : {
-			debug      : process.env.BMWD_DEBUG_PROTOCOL,
-			length_min : 5,
-			length_max : 1000,
-			error_max  : 50,
-		},
-	};
-
 	// Vehicle data bus interface libraries
 	intf = {
 		config : {
-			debug : process.env.BMWD_DEBUG_INTERFACE,
+			debug : process.env.BMWD_DEBUG_INTERFACE || false,
 		},
 		options : {
 			dbus : serial_options('even', false),
@@ -109,12 +102,22 @@ function load_modules(pass) {
 		},
 	};
 
-	// Vehicle data bus protocol and interface libraries
-	// Load these after as they depend on config values above
+	// Vehicle data bus protocol config
+	protocol = {
+		config : {
+			debug      : process.env.BMWD_DEBUG_PROTOCOL || false,
+			length_min : 5,
+			length_max : 1000,
+			error_max  : 50,
+		},
+	};
+
+	// Load separately as they depend on config values above
 	protocol.dbus = require('./protocol/dbus');
 	protocol.ibus = require('./protocol/ibus');
 	protocol.kbus = require('./protocol/kbus');
 
+	// Vehicle interface libs
 	intf.can0 = require('./intf/can0');
 	intf.can1 = require('./intf/can1');
 	intf.dbus = require('./intf/dbus');
@@ -139,11 +142,11 @@ function init() {
 		json.reset(() => { // Reset status vars pertinent to launching app
 			load_modules(() => { // Load IBUS module node modules
 				socket.init(() => { // Open zeroMQ server
-					host_data.init(() => { // Initialize host data object
-						intf[app_intf].init(() => { // Open defined interface
+					// host_data.init(() => { // Initialize host data object
+						// intf[app_intf].init(() => { // Open defined interface
 							log.msg({ msg : 'Initialized' });
-						}, term);
-					}, term);
+						// }, term);
+					// }, term);
 				}, term);
 			}, term);
 		}, term);
