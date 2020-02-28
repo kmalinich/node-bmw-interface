6a6f71559bd4fbe211ba66de6bd903bd652fab22
kdm <kennygprs@gmail.com>
2020-02-28 18:02:39 -0500

    Update

--- isp2-raw.js
+++ isp2-raw.js
@@ -1,3 +1,7 @@
+/* eslint key-spacing      : 0 */
+/* eslint no-console       : 0 */
+/* eslint no-global-assign : 0 */
+
 // https://www.innovatemotorsports.com/support/downloads/Seriallog-2.pdf
 // https://www.innovatemotorsports.com/resources/serial-protocol.php
 // https://arduino.stackexchange.com/questions/70491/decoding-serial-packets-please-check-my-work
@@ -5,6 +9,9 @@
 
 // Lambda is represented as 0.5 to 1.532 in 0.001 increments
 
+
+hex     = require('./share/hex');
+bitmask = require('./share/bitmask');
 
 const SerialPort = require('serialport');
 const ByteLength = require('@serialport/parser-byte-length');
@@ -14,29 +21,53 @@ let cursor = 0;
 
 
 function proc_isp2(buffer) {
-	// The first 2 bytes of a message are the message header
-	// Both bytes of a message header have their high bit set (0x80) ... for what appears to be the purpose of denoting it is the header
-	const header_present = ((Array.from(buffer)[0] & 0x80) && (Array.from(buffer)[1] & 0x80));
+	// console.log('');
 
-	// console.log({ buffer, array, cursor, header_present });
-
-	// Return here if a properly formed header as per the specification is not detected
-	if (!header_present) return;
-
-
-	// The header also contains the total length of a message, which may be modified by any datasource device in the chain
-	// Actual message length can be found by unsetting 0x80 from byte 1
-	const alleged_length = Array.from(buffer)[1] & ~0x80;
-
-	// Return here if alleged length is outside of the bounds of the specification
-	if (alleged_length < 1 || alleged_length > 7) return;
-
+	// console.log({ buffer });
 
 	// Increment cursor by the length of the new buffer data
 	cursor += buffer.length;
 
 	// Add new buffer data to array
 	array = [ ...array, ...Array.from(buffer) ];
+
+	// console.log({ array : Buffer.from(array) });
+
+	// Wait until at least 2 bytes have been accumulated
+	if (array.length < 2) return;
+
+	if (array.length > 6) {
+		array = [];
+		console.log('Emptying array, data is too long');
+		return;
+	}
+
+	// The first 2 bytes of a message are the message header
+	// Both bytes of a message header have their high bit set (0x80) ... for what appears to be the purpose of denoting it is the header
+	const header_present = (array[0] & 0x80) && (array[1] & 0x80);
+
+	// Return here if a properly formed header as per the specification is not detected
+	if (!header_present) {
+		array = [];
+		console.log('Emptying array, header not present');
+		return;
+	}
+
+	// Wait until at least 6 bytes have been accumulated
+	if (array.length < 6) return;
+
+	// The header also contains the total length of a message, which may be modified by any datasource device in the chain
+	// Actual message length can be found by unsetting 0x80 from byte 1
+	const alleged_length = array[1] & ~0x80;
+
+	// console.log({ buffer, array : Buffer.from(array), alleged_length });
+
+	// Empty array and return here if alleged length is outside of the bounds of the specification
+	if (alleged_length < 1 || alleged_length > 7) {
+		array = [];
+		console.log('Emptying array, alleged length %i too long or too short', alleged_length);
+		return;
+	}
 
 
 	while (cursor > 3 && cursor >= (alleged_length + 4)) {
@@ -64,16 +95,60 @@ function proc_isp2(buffer) {
 
 
 		// Func0, Func1, Func2
+		// (byte 0, bit 2+3+4)
 		// Function/status bits indicating how interpret the next word (lambda word)
 		//
-		// 000 - Lambda valid and aux data valid, normal operation
-		// 001 - Warming up, lambda value is temp in 1/10% of operating temp
-		// 010 - Free air calibration in progress, lambda data not valid
-		// 011 - Error code in lambda value
-		// 100 - Lambda value contains O2 level in 1/10%
-		// 101 - Heater calibration, lambda value contains calibration countdown
-		// 110 - Need free air calibration Request, lambda data not valid
-		// 111 - Lambda value is flash level in 1/10%
+		// 0 0 0 - Lambda valid
+		// 0 0 1 - Warming up, lambda value is operating temp in 1/10%
+		// 0 1 0 - Lambda value not valid - free air calibration in progress
+		// 0 1 1 - Lambda value not valid - lambda value is error code
+		// 1 0 0 - Lambda value is O2 level in 1/10%
+		// 1 0 1 - Heater calibration, lambda value contains calibration countdown
+		// 1 1 0 - Lambda value not valid - requires free air calibration
+		// 1 1 1 - Lambda value is flash level in 1/10%
+
+		const mask = bitmask.check(frame[0]).mask;
+
+		let status = 'unknown';
+
+		switch (mask.bit2) {
+			case false : {
+				switch (mask.bit3) {
+					case false : {
+						switch (mask.bit4) {
+							case false : status = 'Lambda valid'; break;
+							case true  : status = 'Warming up, value is %';
+						}
+						break;
+					}
+					case true : {
+						switch (mask.bit4) {
+							case false : status = 'Lambda value not valid - free air calibration in progress'; break;
+							case true  : status = 'Value is error code';
+						}
+					}
+				}
+				break;
+			}
+
+			case true : {
+				switch (mask.bit3) {
+					case false : {
+						switch (mask.bit4) {
+							case false : status = 'Value is O2 %'; break;
+							case true  : status = 'Heater calibration, value is calibration countdown';
+						}
+						break;
+					}
+					case true : {
+						switch (mask.bit4) {
+							case false : status = 'Lambda value not valid - requires free air calibration'; break;
+							case true  : status = 'Value is flash %';
+						}
+					}
+				}
+			}
+		}
 
 
 		// Byte 1, bit 0 : Remaining bit of AFR multiplier (AF0)
@@ -90,13 +165,14 @@ function proc_isp2(buffer) {
 
 		// Bytes 2 and 3 of the message contain lambda or status detail information
 		const data = {
+			status,
 			lambda : (((frame[4] * 128) + frame[5]) / 1000) + 0.5,
 		};
 
 		// Format up the lambda data a little bit
 		data.lambda = parseFloat(data.lambda.toFixed(3));
 
-		console.log({ push : data });
+		console.log(data);
 	}
 }
 
@@ -108,15 +184,11 @@ const port = new SerialPort('/dev/lm2', {
 port.on('error', console.error);
 
 const parser = port.pipe(new ByteLength({
-	length : 6,
+	length : 1,
 }));
 
 parser.on('data', data => {
 	proc_isp2(data);
-
-	// let lambda = (((data[4] * 128) + data[5]) / 1000) + 0.5;
-	// lambda = lambda.toFixed(3);
-	// console.log({ lambda, data });
 }); // will have 16 bytes per data event
 
 
