9569f90c676e5706fdd68d91fa3b5960d9c91d42
Ken Malinich <kmalinich@users.noreply.github.com>
2020-10-07 12:22:50 -0400

    Update

--- main.js
+++ main.js
@@ -66,11 +66,11 @@ async function load_modules() {
 			debug : process.env.BMWI_DEBUG_PROTOCOL || false,
 
 			msg_length_min : 5,
-			msg_length_max : 30,
+			msg_length_max : 60,
 
 			queue_length_max : 1000,
 
-			error_max  : 50,
+			error_max : 50,
 		},
 		proto : null,
 	};
